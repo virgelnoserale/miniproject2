e7a542ffdd63de00c04a8d4ca5d9eadf209520c3
virgelnoserale <jhanhanes@gmail.com>
2022-11-20 21:46:12 +0800

    added capsule page and details buy button

---
+++ js/loadingsetmytime.js
@@ -0,0 +1,5 @@
+setTimeout(function () { 
+    document.querySelector("div.page-loading-status").style = "display: none";
+    
+    document.querySelector("div.container").style = "display: block";
+}, 2000);
