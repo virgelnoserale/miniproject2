692c98e2d1fc932e0a5f0acee452f3eb9c99d549
hellojhonas <jhonacenas0704@gmail.com>
2022-12-02 23:20:38 +0800

    added loading animation and finalized footer

--- js/landing.js
+++ js/landing.js
@@ -1,3 +1,13 @@
+//loading delay
+document.querySelector("div.container-fluid").style = "display: none";
+
+setTimeout(function () { 
+    document.querySelector("div.loading").style = "display: none";
+    
+    document.querySelector("div.container-fluid").style = "display: block";
+}, 3000);
+
+// scrolling subtle animation
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
